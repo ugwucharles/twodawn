ee3f0aafa4280aafd6a012f75838b3d07bc6994b
OCHI <ochi@2dawn.com>
2026-03-05 23:45:09 +0100

    Fix design issues and update branding to 2DAWN

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -66,9 +66,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/jpeg" href="{{ asset('logo.jpg') }}">
     <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
-    <link rel="apple-touch-icon" href="{{ asset('images/Group 2.png') }}">
-    <meta name="apple-mobile-web-app-title" content="{{ $appName }}">
+    <link rel="apple-touch-icon" href="{{ asset('logo.jpg') }}">
+    <meta name="apple-mobile-web-app-title" content="2DAWN">
 
     @php
       $manifestPath = public_path('build/manifest.json');
